b6bd1d79afc18a459243d8c0a0d7358d58f8d9ea
morgancollado <Morgan.collado@gmail.com>
2020-11-22 20:07:54 -0600

    commit tree 21599dfc97913780986267c42ba9f55b4ee7816b

--- index.js
+++ index.js
@@ -3,3 +3,10 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+
+function superbowlWin(array) {
+  let win = array.find(obj => {
+    return obj.result === "W"
+  })
+return win ? win.year : undefined
+}
